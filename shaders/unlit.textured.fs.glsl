82e735fd1082702862ca903d97acec978c5b8a58
Gabe <GabrielL@csumb.edu>
2018-11-26 12:38:20 -0800

    Todo 8 fully functional

--- shaders/unlit.textured.fs.glsl
+++ shaders/unlit.textured.fs.glsl
@@ -6,7 +6,8 @@ uniform float uAlpha;
 varying vec2 vTexCoords; //Todo #2 receive texture coordinates and verify
 
 void main(void) {
-    vec4 texColor = texture2D(uTexture, vTexCoords, uAlpha);
+    vec4 texColor = texture2D(uTexture, vTexCoords);
+    texColor[3] = uAlpha;
     //gl_FragColor = vec4(1,vTexCoords[0],vTexCoords[1],1.0); //Todo #2 receive texture coordinates and verify
     gl_FragColor = (texColor); //Todo #5 switch to texture
 }
