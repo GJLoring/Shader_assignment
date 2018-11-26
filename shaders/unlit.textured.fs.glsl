2f5b94b58bb235d7b40ad8a77e00d928237b3b41
Gabe <GabrielL@csumb.edu>
2018-11-26 12:32:45 -0800

    Add in uAlpha to Shader

--- shaders/unlit.textured.fs.glsl
+++ shaders/unlit.textured.fs.glsl
@@ -6,7 +6,7 @@ uniform float uAlpha;
 varying vec2 vTexCoords; //Todo #2 receive texture coordinates and verify
 
 void main(void) {
-    vec4 texColor = texture2D(uTexture, vTexCoords);
-    gl_FragColor = vec4(1,vTexCoords[0],vTexCoords[1],1.0); //Todo #2 receive texture coordinates and verify
-     gl_FragColor = texColor; //Todo #5 switch to texture
+    vec4 texColor = texture2D(uTexture, vTexCoords, uAlpha);
+    //gl_FragColor = vec4(1,vTexCoords[0],vTexCoords[1],1.0); //Todo #2 receive texture coordinates and verify
+    gl_FragColor = (texColor); //Todo #5 switch to texture
 }
